763a0f71fb031d5d86a8f97f22203c16119b3175
ksvedal <kristoffersvedal@gmail.com>
2022-02-21 13:45:28 +0100

    Added minimum functionality to UDP.Client class

--- src/main/java/UDP/Client.java
+++ src/main/java/UDP/Client.java
@@ -1,4 +1,53 @@
 package UDP;
 
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
 public class Client {
+
+    private DatagramSocket datagramSocket;
+    private InetAddress inetAddress;
+    private final int port = 1251;
+    private byte[] buffer;
+
+    public Client(DatagramSocket datagramSocket, InetAddress inetAddress) {
+        this.datagramSocket = datagramSocket;
+        this.inetAddress = inetAddress;
+    }
+
+    public static void main(String[] args) throws SocketException, UnknownHostException {
+        DatagramSocket datagramSocket = new DatagramSocket();
+        InetAddress inetAddress = InetAddress.getByName("localhost");
+        Client client = new Client(datagramSocket, inetAddress);
+        System.out.println("Send a calculation like this: [1 + 1]");
+        client.sendAndReceive();
+    }
+
+    public void sendAndReceive() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+
+                String messageToSend = scanner.nextLine();
+
+                if (messageToSend.equals("exit"))
+                    break;
+
+                buffer = messageToSend.getBytes();
+                DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length, inetAddress, port);
+                datagramSocket.send(datagramPacket);
+                datagramSocket.receive(datagramPacket);
+                String messageFromServer = new String(datagramPacket.getData(), 0, datagramPacket.getLength());
+
+                System.out.println(messageFromServer);
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
 }
