887ed4093bf396c51d911d9415847867a10014d7
ksvedal <kristoffersvedal@outlook.com>
2022-03-10 14:21:02 +0100

    website now displays header of client trying to connect.

--- src/main/java/TCP/WebServer.java
+++ src/main/java/TCP/WebServer.java
@@ -1,6 +1,8 @@
 package TCP;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,17 +10,28 @@ import java.net.Socket;
 public class WebServer {
     public static void main(String[] args) throws IOException {
         final int port = 80;
+
+        ServerSocket server = new ServerSocket(port);
+        Socket connection = server.accept();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        PrintWriter writer = new PrintWriter(connection.getOutputStream(), true);
+
+        StringBuilder head = new StringBuilder("<br>");
+        for (int i = 0; i < 14; i++) {
+            head.append("<br>").append(reader.readLine());
+        }
+
+        System.out.println(head);
+
         final String htmlPrint =
-                        "HTTP/1.0 200 OK \n" +
+                "HTTP/1.0 200 OK \n" +
                         "Content-Type: text/html; charset=utf-8 \n" +
                         "\n" +
                         "<html> <body>" +
                         "<h1> Web T. Jenner </h1>" +
+                        "Header: " +
+                        head +
                         "</body></html>";
-
-        ServerSocket server = new ServerSocket(port);
-        Socket connection = server.accept();
-        PrintWriter writer = new PrintWriter(connection.getOutputStream(), true);
 
         writer.println(htmlPrint);
         writer.close();
